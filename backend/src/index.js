9712eaebc1be31ab3c396854fc62c01982a37c22
soeunj <thdmsdia@gmail.com>
2019-08-28 13:54:33 +0300

    Fix hardcoded apikey

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,7 @@ const router = require('koa-router')();
 const fetch = require('node-fetch');
 const cors = require('kcors');
 
-const appId = process.env.APPID || '1808f5399099a91d6f78a15381494fbb';
+const appId = process.env.APPID;
 const mapURI = process.env.MAP_ENDPOINT || 'http://api.openweathermap.org/data/2.5';
 const targetCity = process.env.TARGET_CITY || 'Helsinki,fi';
 
